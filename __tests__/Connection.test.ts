9aedde274636e01982691795d212f16110017cd6
NaraFrois <88464177+MitOwO@users.noreply.github.com>
2023-05-24 16:29:44 -0300

    Delete __tests__ directory

--- __tests__/Connection.test.ts
+++
@@ -1,7 +0,0 @@
-import Borzoi from "../src/structures/Borzoi";
-
-test('Borzoi WebUI API', async () => {
-    const code = await new Borzoi({url: "http://127.0.0.1:7860"}).testConnection()
-
-    expect(code).toBe(200)
-})
