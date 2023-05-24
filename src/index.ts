1575b7e461a3f3c6d5f729ced0a93b05edb0832d
MitOwO <yukinetut16@gmail.com>
2023-05-24 17:21:52 -0300

    Some configs

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import Borzoi from "./structures/Borzoi";
+import { BorzoiAPIEndpoints } from "./structures/BorzoiAPIEndpoints";
+import { BorzoiInferences } from "./structures/BorzoiInferences";
+import BorzoiPayloadBuilder from "./structures/BorzoiPayloadBuilder";
+import { BorzoiSampler } from "./structures/BorzoiSampler";
+import { BorzoiUpscaler } from "./structures/BorzoiUpscaler";
+import { OutputPayload } from "./structures/OutputPayload";
+
+export {
+    Borzoi,
+    BorzoiInferences, 
+    BorzoiPayloadBuilder,
+    OutputPayload,
+    BorzoiAPIEndpoints,
+    BorzoiSampler,
+    BorzoiUpscaler,
+}
